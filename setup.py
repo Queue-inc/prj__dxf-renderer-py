b51a5140e74cb1094afcea79bf050931d79dc00a
nkennek <chris.nk.0802@gmail.com>
2019-05-22 14:47:26 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dxfvis',
+    version='0.1',
+    description='Python visualization toll for dxf files',
+    author='Queue-inc.',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'ezdxf>=0.9.0'
+    ]
+)
