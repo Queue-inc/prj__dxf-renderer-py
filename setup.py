f80456fac5239307031277d2bd05c24eee04fbc5
nkennek <chris.nk.0802@gmail.com>
2019-09-05 19:43:02 +0900

    ezdxfのバージョンを固定

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dxfvis',
-    version='0.1',
-    description='Python visualization toll for dxf files',
-    author='Queue-inc.',
+    name="dxfvis",
+    version="0.1",
+    description="Python visualization toll for dxf files",
+    author="Queue-inc.",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'ezdxf>=0.9.0'
-    ]
+    install_requires=["ezdxf==0.9.0"],
 )
